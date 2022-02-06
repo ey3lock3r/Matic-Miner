a129a642e6b8c830c12e2e78e7708c8f8f336cad
Dhinson Dacpano <dhinsondacpano@darkWeb.local>
2022-02-06 21:24:04 +0100

    initial commit

---
+++ Bot.py
@@ -0,0 +1,149 @@
+import asyncio
+import logging
+import requests
+import time
+import traceback
+from web3 import Web3
+from web3.middleware import geth_poa_middleware
+
+class MinerBot():
+    def __init__(self, rpc, gas_api, miner_abi, miner_cont, transaction, pkey, polyscan_token, interval, logger=None):
+        self.rpc = rpc
+        self.gas_api = gas_api + polyscan_token
+        self.miner_abi = miner_abi
+        self.miner_cont = miner_cont
+        self.pkey = pkey
+        self.interval = interval
+        
+        self.transaction = transaction
+        self.hash = None
+
+        self.logger = (logging.getLogger(logger) if isinstance(logger,str) else logger)
+        if self.logger is None:
+            self.logger = logging.getLogger(__name__)
+        
+        web3 = Web3(Web3.HTTPProvider(rpc))
+        web3.middleware_onion.inject(geth_poa_middleware, layer=0)
+        self.account = web3.eth.account.privateKeyToAccount(pkey)
+        self.miner = web3.eth.contract(address=Web3.toChecksumAddress(self.miner_cont), abi=self.miner_abi)
+        self.web3 = web3
+
+        self.logger.info('Miner Bot initialized!')
+
+    def send_transaction(self, tx):
+        # sign the transaction
+        signed_tx = self.web3.eth.account.sign_transaction(tx, self.pkey)
+        # send the transaction
+        self.hash = self.web3.eth.send_raw_transaction(signed_tx.rawTransaction)
+        self.logger.info(f'Tx Hash: {self.hash.hex()}')
+
+    async def execute_trans(self):
+        self.logger.info('>'*70)
+        self.logger.info('Executing...')
+        loop = asyncio.get_event_loop()
+        web3 = self.web3    
+        if self.hash:
+            web3.eth.get_transaction_receipt(self.hash)    # If transaction still pending throws an error
+
+        gasp = int(requests.get(self.gas_api).json()['result']['SafeGasPrice'])
+        if gasp > 100:
+            return
+
+        account = self.account
+        mminer = self.miner
+
+        balance = loop.run_in_executor(None, web3.eth.getBalance, account.address)
+        nonce = loop.run_in_executor(None, web3.eth.getTransactionCount, account.address)
+        gasPrice = loop.run_in_executor(None, web3.toWei, '30', 'gwei')
+        eggs = loop.run_in_executor(None, mminer.functions.getMyEggs().call, {'from': account.address})
+
+        balance = await balance
+        eggs = await eggs
+        maticBal = loop.run_in_executor(None, web3.fromWei, balance, "ether")
+        mined_matic = loop.run_in_executor(None, mminer.functions.calculateEggSell(eggs).call)
+
+        maticBal = await maticBal
+        nonce = await nonce
+        gasPrice = await gasPrice
+        mined_matic = await mined_matic
+        mined_matic_1 = web3.fromWei(mined_matic, "ether")
+
+        self.logger.info(f'Balance: {maticBal}')
+        self.logger.info(f'Mined Matic: {mined_matic_1}')
+        self.logger.info(f'Gas Price: {gasp}')
+        
+        tx = {
+            'nonce': nonce,
+            'from': account.address,
+            'gasPrice': gasPrice
+        }
+        tx = {**self.transaction, **tx}
+
+        # >>>> Autocompound
+        if maticBal >= 0.01:
+            # Calculate Gas Fee
+            estGas = mminer.functions.hatchEggs(Web3.toChecksumAddress(account.address)).estimateGas(tx)
+            gasFee = estGas * gasPrice
+            # tx['gas'] = estGas
+
+            if mined_matic - gasFee > gasFee:
+                gasFee_1 = web3.fromWei(gasFee, "ether")
+                self.logger.info(f'Gas Fee: {gasFee_1}')
+                self.logger.info('Autocompounding...')
+                # build the transaction
+                tx_built = mminer.functions.hatchEggs(Web3.toChecksumAddress(account.address)).buildTransaction(tx)
+                self.send_transaction(tx_built)
+
+        # >>>> Withdraw Matic
+        else:
+            # Calculate Gas & Dev Fee
+            estGas = loop.run_in_executor(None, mminer.functions.sellEggs().estimateGas, tx)
+            devFee = loop.run_in_executor(None, mminer.functions.devFee(eggs).call)
+            gasFee = await estGas * gasPrice
+            devFee = await devFee
+            
+            # devFeeMatic = mminer.functions.calculateEggSell(devFee).call()
+            earned_matic = mminer.functions.calculateEggSell(eggs - devFee).call()
+
+            actualDevFee = mined_matic - earned_matic
+            totalFees = gasFee + actualDevFee
+
+            if earned_matic - gasFee > totalFees:
+                gasFee_1 = loop.run_in_executor(None, web3.fromWei, gasFee, "ether")
+                actualDevFee_1 = loop.run_in_executor(None, web3.fromWei, actualDevFee, "ether")
+                gasFee_1 = await gasFee_1
+                actualDevFee_1 = await actualDevFee_1
+                self.logger.info(f'Gas Fee: {gasFee_1}')
+                self.logger.info(f'Dev Fee: {actualDevFee_1}')
+                self.logger.info('Withdrawing...')
+                # build the transaction
+                tx_built = mminer.functions.sellEggs().buildTransaction(tx)
+                self.send_transaction(tx_built)
+
+    async def start(self, interval):
+        
+        while True:
+            # sleep until the next whole interval
+            await asyncio.sleep(interval - time.time() % interval)
+
+            try:
+                await self.execute_trans()
+            except Exception as E:
+                self.logger.info(f'Error: {E}')
+                self.logger.debug(traceback.print_exc())
+
+    def run(self):
+        """Wrapper for start to run without additional libraries for managing asynchronous"""
+
+        loop = asyncio.get_event_loop()
+        try:
+            loop.run_until_complete(self.start(self.interval))
+
+        except KeyboardInterrupt:
+            self.logger.info('Gracefully exit')
+
+        finally:
+            loop.close()
+            self.logger.info('Program finished')
+
+
