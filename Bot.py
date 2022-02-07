d0113cb6ae80670c7552e00202a6de59288d3acd
Dhinson Dacpano <dhinsondacpano@darkWeb.local>
2022-02-07 17:05:02 +0100

    try catch on get_transaction_receipt and skip if gas price > 50

--- Bot.py
+++ Bot.py
@@ -42,11 +42,15 @@ class MinerBot():
         self.logger.info('Executing...')
         loop = asyncio.get_event_loop()
         web3 = self.web3    
-        if self.hash:
+        # if self.hash:
+        try:
             web3.eth.get_transaction_receipt(self.hash)    # If transaction still pending throws an error
+        except Exception as E:
+            self.logger.info(f'Error: {E}')
+            return
 
         gasp = int(requests.get(self.gas_api).json()['result']['SafeGasPrice'])
-        if gasp > 100:
+        if gasp > 50:
             return
 
         account = self.account
